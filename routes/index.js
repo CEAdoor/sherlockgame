489e817995a5febbb9c0b52244f737cd77f06816
ashikmeerankutty <ashik9591@gmail.com>
2018-02-21 19:48:46 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,16 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/',userController.home);
-router.get('/login',authController.isNotLoggedIn,userController.loginForm);
-router.post('/login', authController.login);
-router.get('/play',authController.isLoggedIn,playController.play);
-router.post('/play',playController.newlevel);
-router.get('/register',authController.isNotLoggedIn,userController.registerForm);
-router.post('/register',
-    userController.validateRegister,
-    catchErrors(userController.register),
-    authController.flogin);
-router.get('/logout',authController.logout);
-router.get('/rules',userController.rules);
-router.get('/about',userController.about);
+// router.get('/login',authController.isNotLoggedIn,userController.loginForm);
+// router.post('/login', authController.login);
+// router.get('/play',authController.isLoggedIn,playController.play);
+// router.post('/play',playController.newlevel);
+// router.get('/register',authController.isNotLoggedIn,userController.registerForm);
+// router.post('/register',
+//     userController.validateRegister,
+//     catchErrors(userController.register),
+//     authController.flogin);
+// router.get('/logout',authController.logout);
+// router.get('/rules',userController.rules);
+// router.get('/about',userController.about);
 module.exports = router;
