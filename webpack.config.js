e1b6420d010045dbf39b725f3c1dad150f557125
ashikmeerankutty <ashik9591@gmail.com>
2018-03-16 21:13:20 +0530

    updated readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -46,23 +46,6 @@ const uglify = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
   compress: { warnings: false }
 });
 
-// OK - now it's time to put it all together
-const config = {
-  entry: {
-    // we only have 1 entry, but I've set it up for multiple in the future
-    App: './public/javascripts/delicious-app.js'
-  },
-  // we're using sourcemaps and here is where we specify which kind of sourcemap to use
-  devtool: 'source-map',
-  // Once things are done, we kick it out to a file.
-  output: {
-    // path is a built in node module
-    // __dirname is a variable from node that gives us the
-    path: path.resolve(__dirname, 'public', 'dist'),
-    // we can use "substitutions" in file names like [name] and [hash]
-    // name will be `App` because that is what we used above in our entry
-    filename: '[name].bundle.js'
-  },
 
   // remember we said webpack sees everthing as modules and how different loaders are responsible for different file types? Here is is where we implement them. Pass it the rules for our JS and our styles
   module: {
